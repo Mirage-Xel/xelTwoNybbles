44587d60063a85d4ea82122d22643e6ba74208f1
MirageXel <joekingyur@gmail.com>
2021-07-29 17:37:57 +0300

    Organization fixes + Code cleanup by Emik

--- Two Nybbles/Assets/Two Nybbles/Scripts/TwoNybbles.cs
+++ Two Nybbles/Assets/Two Nybbles/Scripts/TwoNybbles.cs
@@ -3,88 +3,88 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using rnd = UnityEngine.Random;
+using Rnd = UnityEngine.Random;
 using KModkit;
 using KeepCoding;
 public class TwoNybbles : ModuleScript
 {
-    public KMBombModule module;
-    public KMAudio sound;
+    private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public KMSelectable[] specialButtons;
-    public KMSelectable[] buttons;
-    public TextMesh display;
+    [SerializeField] internal KMSelectable[] SpecialButtons;
+    [SerializeField] internal KMSelectable[] Buttons;
+    [SerializeField] private TextMesh display; 
 
-    string[] buttonTexts = new string[12];
-    List<int> queriedButtons = new List<int>();
+    private string[] buttonTexts = new string[12];
+    private List<int> queriedButtons = new List<int>();
     bool processing;
-    bool solved;
-
 
     void Start()
     {
         for (int i = 0; i < 12; i++)
         {
-            buttonTexts[i] = Helper.Alphanumeric[rnd.Range(10, 36)] + Helper.Decimal[rnd.Range(0, 10)].ToString();
-            buttons[i].GetComponentInChildren<TextMesh>().text = buttonTexts[i];
+            buttonTexts[i] = Alphabet.PickRandom().ToString() + Helper.Decimal.PickRandom();
+            Buttons[i].GetComponentInChildren<TextMesh>().text = buttonTexts[i];
         }
-        buttons.Assign(onInteract: x => OnButtonPress(ArrayHelper.IndexOf<KMSelectable>(buttons, x)));
-        specialButtons.Assign(onInteract: x => OnSpecialButtonPress(ArrayHelper.IndexOf<KMSelectable>(specialButtons, x)));
+        Buttons.Assign(onInteract: x => OnButtonPress(Buttons.IndexOf<KMSelectable>(x)));
+        SpecialButtons.Assign(onInteract: x => OnSpecialButtonPress(SpecialButtons.IndexOf<KMSelectable>(x)));
     }
 
     void OnButtonPress(int index)
     {
-        ButtonEffect(buttons[index], 1, KMSoundOverride.SoundEffect.ButtonPress);
-        StartCoroutine(AnimKeys(buttons[index], new Vector3(0f, 0f, 0.001f)));
-        if (!(solved || processing) && queriedButtons.Count < 3)
-        {
-            if (queriedButtons.Count == 0) display.text = "QUERY:";
-            display.text += " " + buttonTexts[index]; ;
-            queriedButtons.Add(index);
-        }
+        ButtonEffect(Buttons[index], 1, Sound.ButtonPress);
+        StartCoroutine(AnimKeys(Buttons[index], new Vector3(0f, 0f, 0.001f)));
+
+        if (IsSolved || processing || queriedButtons.Count >= 3)
+            return;
+
+            if (queriedButtons.Count == 0)
+                display.text = "QUERY:";
+       
+        display.text += " " + buttonTexts[index];
+        queriedButtons.Add(index);
     }
 
     void OnSpecialButtonPress(int index)
     {
-        ButtonEffect(specialButtons[index], 1, KMSoundOverride.SoundEffect.ButtonPress);
-        StartCoroutine(AnimKeys(specialButtons[index], new Vector3(0f, 0.001f, 0f)));
-        if (!(solved || processing) && queriedButtons.Count != 0)
-        {
+        ButtonEffect(Buttons[index], 1, Sound.ButtonPress);
+        StartCoroutine(AnimKeys(SpecialButtons[index], new Vector3(0f, 0.001f, 0f)));
+
+        if (IsSolved || processing || queriedButtons.Count == 0)
+            return;    
+
             PlaySound("Processing");
-            StartCoroutine(ProcessingAnimation(index));
-        }
+            StartCoroutine(ProcessingAnimation(index));    
     }
 
     string GetQueryResult()
     {
         string result = "F4 20";
-        string query = "";
-        queriedButtons.ForEach(x => query += " " + buttonTexts[x]);
-        Log("You queried{0}, which returned {1}.", query, result);
+        List<String> query = new List<String>();
+        queriedButtons.ForEach(x => query.Add(buttonTexts[x]));
+        Log("You queried {0}, which returned {1}.", query, result);
         return result;
     }
 
     void HandleSubmission()
     {
         //Dummy while module logic is not implemented.
-        string query = "";
-        queriedButtons.ForEach(x => query += " " + buttonTexts[x]);
-        Log("You submitted{0}.", query);
-        if (IsSumbissionCorrect())
+        List<String> query = new List<String>();
+        queriedButtons.ForEach(x => query.Add(buttonTexts[x]));
+        Log("You submitted {0}.", query.UnwrapToString(false, " "));
+
+        if (IsSubmissionCorrect())
         {
             Solve("That was correct. Module solved.");
             PlaySound(KMSoundOverride.SoundEffect.CorrectChime);
             display.text = "CORRECT. SOLVED";
-            solved = true;
+            return;
         }
-        else
-        {
+
             Strike("That was incorrect. Strike!");
             display.text = "WRONG. STRIKE.";
-        }
     }
 
-    bool IsSumbissionCorrect()
+    bool IsSubmissionCorrect()
     {
         //Dummy while module logic is not implemented.
         return false;
@@ -92,15 +92,19 @@ public class TwoNybbles : ModuleScript
 
     IEnumerator ProcessingAnimation(int state)
     {
+        Debug.Log(queriedButtons.Count());
         int periodCount = 0;
-        List<float> delayTimes = Enumerable.Repeat(0.416666667f, 12).ToList();
+        List<float> delayTimes = Enumerable.Repeat(5 / 12f, 12).ToList();
+
         for (int i = 0; i < 12; i += 2)
         {
-            float param = rnd.Range(0f, 0.4f);
+            float param = Rnd.Range(0f, 0.4f);
             delayTimes[i] += param;
             delayTimes[i + 1] -= param;
         }
+
         delayTimes.Shuffle();
+
         for (int i = 0; i < 12; i++)
         {
             display.text = "PROCCESING" + (Enumerable.Repeat(".", periodCount)).Join();
@@ -108,6 +112,7 @@ public class TwoNybbles : ModuleScript
             if (periodCount == 4) periodCount = 0;
             yield return new WaitForSeconds(delayTimes[i]);
         }
+
         switch (state)
         {
             case 0:
@@ -117,8 +122,10 @@ public class TwoNybbles : ModuleScript
                 HandleSubmission();
                 break;
         }
+
         queriedButtons.Clear();
     }
+
     IEnumerator AnimKeys(KMSelectable button, Vector3 vec)
     {
         {
@@ -127,6 +134,7 @@ public class TwoNybbles : ModuleScript
                 button.transform.localPosition -= vec;
                 yield return new WaitForSeconds(0.02f);
             }
+
             for (int i = 0; i < 3; i++)
             {
                 button.transform.localPosition += vec;
@@ -134,6 +142,5 @@ public class TwoNybbles : ModuleScript
             }
         }
     }
-
 }
 
--- Two Nybbles/Assets/Two Nybbles/Scripts/TwoNybblesTP.cs
+++ Two Nybbles/Assets/Two Nybbles/Scripts/TwoNybblesTP.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using KeepCoding;
 
@@ -15,39 +16,47 @@ public class TwoNybblesTP: TPScript<TwoNybbles> {
     {
         string[] cmdArray = command.ToLowerInvariant().Split(" ");
         List<int> pressedButtons = new List<int>();
-        if (!IsMatch(cmdArray[0], "(query|submit)")) yield break;
+
+        if (!IsMatch(cmdArray[0], "(query|submit)"))
+           yield break;
+
         yield return null;
+
         if (cmdArray.Length == 1)
         {
             yield return SendToChatError("Please specify which buttons to press!");
             yield break;
         }
+
         if (cmdArray.Length > 4)
         {
             yield return SendToChatError("Too many parameters!");
             yield break;
         }
-        foreach (string i in cmdArray)
+
+        foreach (string i in cmdArray.Skip(1).ToArray())
         {
-            if (!IsMatch(i, "(query|submit)"))
-            {
                 int button;
+
                 if (!int.TryParse(i.ToString(), out button))
                 {
                     yield return SendToChatError(("Parameter '{0}' not a number!").Form(i));                   
                     yield break;
                 }
+
                 if (button < 1 || button > 12)
                 {
                     yield return SendToChatError(("Parameter '{0}' out of range!").Form(i));
                     yield break;
                 }
+
                 pressedButtons.Add(button - 1);        
-            }          
+                  
         }
-        yield return OnInteractSequence(Module.buttons, 1.5f, pressedButtons.ToArray());
+
+        yield return OnInteractSequence(Module.Buttons, 1.5f, pressedButtons.ToArray());
         yield return new WaitForSeconds(1.5f);
         int specialButton = !IsMatch(cmdArray[0], "query") ? 1 : 0;
-        Module.specialButtons[specialButton].OnInteract();
+        Module.SpecialButtons[specialButton].OnInteract();
     }
 }
